e8ba73c075ce8883bea667071ba79e4ebf7fb1cd
Erin Truman <erintruman@Erins-MacBook-Pro.local>
2021-07-23 14:28:54 -0500

    I added an even listener that shows who is walking what pet

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,43 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
 
 const pets = getPets()
+const walkers = getWalkers()
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if ( pet.id === parseInt(petId)) {
+
+
+                    const walker = walkers.find(walker => walker.id === pet.walkerId)
+
+
+                    window.alert(`${pet.name} is being walked by ${walker.name}.`)
+                }
+            }
+        }
+    }
+)
+
+
+
+
+
+
+
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
